1c5e7b79e2cd860ce6235de40da7ab6acd467ec3
lzh <2024510052@qq.com>
2026-06-21 18:35:49 +0800

    创建项目框架

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/hello/{name}")
+async def say_hello(name:str):
+    return {"message": f"Hello {name}"}
