3313074da9e3cd2fc47ef7745db98ed5b457bfe5
lzh <2024510052@qq.com>
2026-06-22 18:02:12 +0800

    写获取分类路由

--- routers/news.py
+++ routers/news.py
@@ -2,7 +2,12 @@ from fastapi import APIRouter
 
 # 创建 APIRuter 实例
 router = APIRouter(prefix='/api/news',tags=["news"])
+# prefix 是公共的url前缀，会自动补全下面所有的路由方法中的url，不需要再重复写
 
-@router.get("/categories")
-async def get_categories():
-    return {"msg":"获取分类成功"}
+@router.get("/categories")  # 这里的url相当于/api/news/categories
+async def get_categories(skip: int=0, limit: int=100):
+    return {
+        "code": 200,
+        "message": "success",
+        "data": "新闻分类列表"
+    }
