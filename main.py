3c09f3bbeb8fc509cea66eb5890721e4f259a24a
lzh <2024510052@qq.com>
2026-06-23 16:18:15 +0800

    解决跨域问题，同时把后端文件夹修改回来了，处理了一些bug,现在这个版本可以稳定访问分类信息并返回

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from routers import news
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://localhost:8080"], # 允许前端的地址
+    allow_credentials=True,
+    allow_methods=["*"], # 允许所有请求方法 (GET, POST 等)
+    allow_headers=["*"], # 允许所有请求头
+)
 
 @app.get("/")
 async def root():
