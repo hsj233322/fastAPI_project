dd1bb6d104297cf6719305234419c79849b27d0e
lzh <2024510052@qq.com>
2026-06-24 09:10:11 +0800

    获取详情页，并且增加浏览量

--- crud/news.py
+++ crud/news.py
@@ -1,8 +1,42 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from models.news import Category
+from models.news import Category, News
+from sqlalchemy import func, update
 
 async def get_categories(db: AsyncSession, skip: int=0, limit: int=100):
     stmt = select(Category).offset(skip).limit(limit)
     result = await db.execute(stmt)
     return result.scalars().all()
+
+async def get_news_list(db: AsyncSession, category_id: int, skip: int=0, limit: int=10):
+    # 查询指定分类下的所有新闻
+    stmt = select(News).where(News.category_id == category_id).offset(skip).limit(limit)
+    result = await db.execute(stmt)
+    return result.scalars().all()
+
+async def get_news_count(db: AsyncSession, category_id: int):
+    # 查询指定分类下的新闻数量
+    stmt = select(func.count(News.id)).where(News.category_id == category_id)
+    result = await db.execute(stmt)
+    return result.scalar_one()  # 只能有一个结果，否则报错
+
+async def get_news_detail(db: AsyncSession, news_id: int) -> News | None:
+    stmt = select(News).where(News.id == news_id)
+    result = await db.execute(stmt)
+    return result.scalar_one_or_none()
+
+async def increase_news_views(db: AsyncSession, news_id: int) -> bool:
+    # 查出这条新闻
+    stmt = select(News).where(News.id == news_id)
+    result = await db.execute(stmt)
+    news_item = result.scalar_one_or_none()
+    
+    # 判空
+    if not news_item:
+        return False # 新闻不存在，更新失败
+        
+    # 直接给对象的属性 +1，SQLAlchemy 会在 commit 时自动生成 UPDATE 语句
+    news_item.views += 1
+    await db.commit()
+    
+    return True
--- routers/news.py
+++ routers/news.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query, HTTPException
 from crud import news
 from sqlalchemy.ext.asyncio import AsyncSession
 from config.db_config import get_db
+from typing import Annotated
 
 # 创建 APIRuter 实例
 router = APIRouter(prefix='/api/news',tags=["news"])
@@ -16,4 +17,59 @@ async def get_categories(skip: int=0, limit: int=100, db: AsyncSession = Depends
         "code": 200,
         "message": "success",
         "data": categories
+    }
+
+@router.get("/list")
+async def get_news(
+    category_id: Annotated[int, Query(alias="categoryId")],
+    page: Annotated[int, Query(ge=1)] = 1,
+    page_size: Annotated[int, Query(gt=0, le=100, alias="pageSize")] = 10,
+    db : AsyncSession = Depends(get_db)
+):
+    # 先处理分页规则 → 查询新闻列表（数据库操作） → 计算总量（数据库操作） → 计算是否还有更多
+    offset =  (page-1)* page_size
+    news_list = await news.get_news_list(db, category_id, offset, page_size)
+    
+    total = await news.get_news_count(db, category_id)
+
+    has_more = (offset + len(news_list)) < total
+    
+    return {
+        "code": 200,
+        "message": "获取新闻列表成功",
+        "data": {
+            "list": news_list,
+            "total": total,
+            "hasMore": has_more
+        }
+    }
+
+@router.get("/detail")
+async def get_news_datail(news_id: Annotated[int, Query(alias="id")], db: AsyncSession = Depends(get_db)):
+
+    # 获取新闻详情 + 浏览量+1 + 相关新闻
+    news_detail = await news.get_news_detail(db, news_id)
+
+    if not news_detail:
+        raise HTTPException(status_code=404, detail="新闻不存在")
+
+    views_res = await news.increase_news_views(db, news_detail.id)
+
+    if not views_res:
+        raise HTTPException(status_code=404, detail="新闻不存在")
+
+    return {
+        "code": 200,
+        "message": "success",
+        "data":{
+            "id": news_detail.id,
+            "title": news_detail.title,
+            "content": news_detail.content,
+            "image": news_detail.image,
+            "author": news_detail.author,
+            "publishTime": news_detail.publish_time,
+            "categoryId": news_detail.category_id,
+            "views": news_detail.views,
+            "relatedNews": [] 
+        }
     }
